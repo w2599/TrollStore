c4ea36f1c9c72f8582903033712c4874f40c31bc
w2599 <114452622+w2599@users.noreply.github.com>
2026-01-15 09:51:14 +0800

    cn.zqbb.TStore

--- Shared/TSUtil.h
+++ Shared/TSUtil.h
@@ -17,7 +17,7 @@
 #else
 #define TS_ACTIVE_MARKER TS_MARKER
 #define TS_INACTIVE_MARKER TS_LITE_MARKER
-#define APP_ID @"com.zqbb.TrollStore"
+#define APP_ID @"cn.zqbb.TStore"
 #define APP_NAME TS_NAME
 #define OTHER_APP_NAME TS_LITE_NAME
 #endif
